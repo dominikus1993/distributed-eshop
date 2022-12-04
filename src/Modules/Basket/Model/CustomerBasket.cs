b6dafe557074e724785b0b584d0b315ba79aaf6d
Dominik.Kotecki <dominikus1910@gmail.com>
2022-12-04 14:23:15 +0100

    fix

--- src/Modules/Basket/Model/CustomerBasket.cs
+++ src/Modules/Basket/Model/CustomerBasket.cs
@@ -4,10 +4,34 @@ namespace Basket.Model;
 
 public readonly record struct CustomerId(Guid Value);
 
-public readonly record struct EmptyBasket(CustomerId CustomerId0); 
+public readonly record struct EmptyBasket(CustomerId CustomerId);
+
+public readonly record struct ItemId(Guid Id);
+
+public readonly record struct ItemQuantity(uint Value);
+
+public sealed record BasketItem(ItemId ItemId, ItemQuantity Quantity);
+
+public sealed class ActiveBasket
+{
+    public required CustomerId CustomerId { get; init; }
+    public required IReadOnlyCollection<BasketItem> Items { get; init; }
+
+    private ActiveBasket()
+    {
+        
+    }
+
+    public static ActiveBasket Zero(CustomerId customerId)
+    {
+        return new ActiveBasket { CustomerId = customerId, Items = Array.Empty<BasketItem>() };
+    } 
+}
+
+
 
 [GenerateOneOf]
-public sealed partial class CustomerBasket: OneOfBase<EmptyBasket>
+public sealed partial class CustomerBasket: OneOfBase<EmptyBasket, ActiveBasket>
 {
     
 }
