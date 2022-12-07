7527a1f6d981a0bc507dd2610b3f7253a836a0e0
Dominik.Kotecki <dominikus1910@gmail.com>
2022-12-07 18:30:25 +0100

    add remove item func

--- src/Modules/Basket/Model/CustomerBasket.cs
+++ src/Modules/Basket/Model/CustomerBasket.cs
@@ -24,12 +24,24 @@ public readonly partial struct ItemId
     
 }
 
-public readonly record struct ItemQuantity(uint Value) : IAdditionOperators<ItemQuantity, ItemQuantity, ItemQuantity>
+public readonly record struct ItemQuantity(uint Value) : IAdditionOperators<ItemQuantity, ItemQuantity, ItemQuantity>, ISubtractionOperators<ItemQuantity, ItemQuantity, ItemQuantity>
 {
     public static readonly ItemQuantity Zero = new(0);
+    public bool IsZero => Value == 0;
+    
     public static ItemQuantity operator +(ItemQuantity left, ItemQuantity right)
     {
         return new ItemQuantity(left.Value + right.Value);
+    }
+
+    public static ItemQuantity operator -(ItemQuantity left, ItemQuantity right)
+    {
+        if (right.Value > left.Value)
+        {
+            return Zero;
+        }
+
+        return new (left.Value - right.Value);
     }
 }
 
@@ -44,6 +56,8 @@ public sealed record BasketItem(ItemId ItemId, ItemQuantity Quantity)
 
         return other.ItemId == ItemId;
     }
+
+    public bool HasElements => !Quantity.IsZero;
     
     public override int GetHashCode()
     {
@@ -54,13 +68,18 @@ public sealed record BasketItem(ItemId ItemId, ItemQuantity Quantity)
     {
         return this with { Quantity = this.Quantity + quantity };
     }
+
+    public BasketItem DecreaseQuantity(ItemQuantity itemQuantity)
+    {
+        return this with { Quantity = this.Quantity - itemQuantity };
+    }
 }
 
 public sealed record BasketItems(IReadOnlyCollection<BasketItem> Items)
 {
     public bool IsEmpty => Items.Count == 0;
 
-    public static BasketItems Empty() => new(Array.Empty<BasketItem>());
+    public static BasketItems Empty => new(Array.Empty<BasketItem>());
     public BasketItems AddItem(BasketItem item)
     {
         var items = new List<BasketItem>(Items);
@@ -78,18 +97,49 @@ public sealed record BasketItems(IReadOnlyCollection<BasketItem> Items)
 
         return new BasketItems(Items: items);
     }
+    
+    public BasketItems RemoveOrDecreaseItem(BasketItem item)
+    {
+        var items = new List<BasketItem>(Items);
+        var index = items.IndexOf(item);
+        if (index == -1)
+        {
+            return this;
+        }
+
+        var oldItem = items[index];
+        var newItem = oldItem.DecreaseQuantity(item.Quantity);
+        if (newItem.HasElements)
+        {
+            items[index] = newItem;  
+        }
+        else
+        {
+            items.RemoveAt(index);
+        }
+
+        return new BasketItems(Items: items);
+    }
 }
 
 public sealed record ActiveBasket(CustomerId CustomerId, BasketItems Items)
 {
+    public bool HasItems => !Items.IsEmpty;
+    
     public static ActiveBasket Zero(CustomerId customerId)
     {
-        return new ActiveBasket(customerId, BasketItems.Empty());
+        return new ActiveBasket(customerId, BasketItems.Empty);
     }
     
     public ActiveBasket AddItem(BasketItem item)
     {
         var items = Items.AddItem(item);
+        return this with { Items = items };
+    }
+    
+    public ActiveBasket RemoveOrDecreaseItem(BasketItem item)
+    {
+        var items = Items.RemoveOrDecreaseItem(item);
         return this with { Items = items };
     }
 }
@@ -105,6 +155,20 @@ public sealed partial class CustomerBasket: OneOfBase<EmptyBasket, ActiveBasket>
     {
         return Match(b => ActiveBasket.Zero(b.CustomerId).AddItem(item), b => b.AddItem(item));
     }
+    
+    public CustomerBasket RemoveItem(BasketItem item)
+    {
+        return Match<CustomerBasket>(b => b, b =>
+        {
+            var newBasket = b.RemoveOrDecreaseItem(item);
+            if (newBasket.HasItems)
+            {
+                return newBasket;
+            }
 
-    public BasketItems GetItems() => Match(e => BasketItems.Empty(), b => b.Items);
+            return EmptyBasket.Zero(b.CustomerId);
+        });
+    }
+
+    public BasketItems GetItems() => Match(_ => BasketItems.Empty, b => b.Items);
 }
