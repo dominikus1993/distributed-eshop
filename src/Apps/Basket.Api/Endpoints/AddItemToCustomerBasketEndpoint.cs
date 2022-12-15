df79431d92629fea2e30e8118a045b7dee5156f4
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-12-15 16:05:16 +0100

    add additem endpoint

---
+++ src/Apps/Basket.Api/Endpoints/AddItemToCustomerBasketEndpoint.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json.Serialization;
+
+using Basket.Core.Model;
+using Basket.Core.Requests;
+
+using FastEndpoints;
+
+using FluentValidation;
+
+using Hosting.Extensions;
+
+using Mediator;
+
+namespace Basket.Api.Endpoints;
+
+public sealed class AddItemToCustomerBasketRequest
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+}
+
+[JsonSerializable(typeof(AddItemToCustomerBasketRequest))]
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
+public partial class AddItemToCustomerBasketEndpointCtx : JsonSerializerContext
+{
+    
+}
+
+
+public sealed class AddItemToBasketRequestValidator : Validator<AddItemToCustomerBasketRequest>
+{
+    public AddItemToBasketRequestValidator()
+    {
+        RuleFor(x => x.Id).GreaterThan(0).WithMessage("Invalid product id");
+        RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("invalid quantity, should be grater than 0");
+    }
+}
+
+
+public sealed class AddItemToCustomerBasketEndpoint : Endpoint<AddItemToCustomerBasketRequest, string>
+{
+    private readonly ISender _sender;
+
+    public AddItemToCustomerBasketEndpoint(ISender sender)
+    {
+        _sender = sender;
+    }
+    
+    public override void Configure()
+    {
+        Post("/api/Basket/items");
+        SerializerContext(AddItemToCustomerBasketEndpointCtx.Default);
+        Validator<AddItemToBasketRequestValidator>();
+        Description(b => b
+            .WithDescription("Add item to basket")
+            .Accepts<AddItemToCustomerBasketRequest>("application/json")
+            .ProducesProblemFE(500)
+            .Produces(200)
+        );
+        DontThrowIfValidationFails();
+    }
+    
+    public override async Task HandleAsync(AddItemToCustomerBasketRequest r, CancellationToken c)
+    {
+        var userId = User.UserId();
+        if (ValidationFailed)
+        {
+            base.Logger.LogWarning("Invalid request");
+            await SendErrorsAsync(cancellation: c);
+            return;
+        }
+        
+        await _sender.Send(new AddItemToCustomerBasket(new CustomerId(userId), new BasketItem(new ItemId(r.Id), new ItemQuantity((uint)r.Quantity))), c);
+        
+        await SendOkAsync(cancellation: c);
+    }
+    
+}
