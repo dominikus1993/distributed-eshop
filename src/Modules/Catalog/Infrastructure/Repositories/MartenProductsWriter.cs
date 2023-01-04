806b3ee22d3f316e02f8c8adb72c37a7bae799e4
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-04 19:25:15 +0100

    .

--- src/Modules/Catalog/Infrastructure/Repositories/MartenProductsWriter.cs
+++ src/Modules/Catalog/Infrastructure/Repositories/MartenProductsWriter.cs
@@ -9,7 +9,21 @@ namespace Catalog.Infrastructure.Repositories;
 
 public sealed class UnableToWriteRecordException : Exception
 {
+    public ProductId ProductId { get; }
+    public UnableToWriteRecordException(ProductId productId) : base("can't write product to database")
+    {
+        ProductId = productId;
+    }
     
+    public UnableToWriteRecordException(ProductId productId, Exception innerException) : base("can't write product to database", innerException)
+    {
+        ProductId = productId;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, {nameof(ProductId)}: {ProductId}";
+    }
 } 
 public sealed class MartenProductsWriter : IProductsWriter
 {
@@ -25,7 +39,7 @@ public sealed class MartenProductsWriter : IProductsWriter
     {
         _store.Products.Add(new EfProduct(product));
         var countOfInsertedRecords = await _store.SaveChangesAsync(cancellationToken);
-        return countOfInsertedRecords == 1 ? Unit.Value : new UnableToWriteRecordException();
+        return countOfInsertedRecords == 1 ? Unit.Value : new UnableToWriteRecordException(product.Id);
     }
 
     public Task<AddProductResult> AddProducts(IEnumerable<Product> products, CancellationToken cancellationToken = default)
