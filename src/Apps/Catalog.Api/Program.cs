2479e9ea5da7ddbe10fdfb087ad6320ebd1ef03d
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-04 19:11:59 +0100

    fix

--- src/Apps/Catalog.Api/Program.cs
+++ src/Apps/Catalog.Api/Program.cs
@@ -1,6 +1,25 @@
+
+using Catalog.Infrastructure.Extensions;
+
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddJWTBearerAuth(builder.Configuration["Security:Jwt:Secret"]!);
+builder.Services.AddSwaggerDoc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpLogging();
 
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseFastEndpoints();
+app.UseSwaggerGen();
 app.Run();
+
+public partial class Program {}
