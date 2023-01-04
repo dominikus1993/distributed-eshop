cee07a294cd8c4c6158a05db3208882f533175d1
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-04 19:21:53 +0100

    fix

--- src/Modules/Catalog/Infrastructure/Repositories/MartenProductsWriter.cs
+++ src/Modules/Catalog/Infrastructure/Repositories/MartenProductsWriter.cs
@@ -1,9 +1,16 @@
 using Catalog.Core.Model;
 using Catalog.Core.Repository;
 using Catalog.Infrastructure.DbContexts;
+using Catalog.Infrastructure.Model;
+
+using Mediator;
 
 namespace Catalog.Infrastructure.Repositories;
 
+public sealed class UnableToWriteRecordException : Exception
+{
+    
+} 
 public sealed class MartenProductsWriter : IProductsWriter
 {
     private readonly ProductsDbContext _store;
@@ -14,9 +21,11 @@ public sealed class MartenProductsWriter : IProductsWriter
         _store = store;
     }
 
-    public Task<AddProductResult> AddProduct(Product product, CancellationToken cancellationToken = default)
+    public async Task<AddProductResult> AddProduct(Product product, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        _store.Products.Add(new EfProduct(product));
+        var countOfInsertedRecords = await _store.SaveChangesAsync(cancellationToken);
+        return countOfInsertedRecords == 1 ? Unit.Value : new UnableToWriteRecordException();
     }
 
     public Task<AddProductResult> AddProducts(IEnumerable<Product> products, CancellationToken cancellationToken = default)
