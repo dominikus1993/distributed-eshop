ced565ba204251824659672a8e45e9a52f9545c2
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-05 19:25:30 +0100

    add to product tests

--- tests/Modules/Catalog.Tests/Infrastructure/Model/EfProductTests.cs
+++ tests/Modules/Catalog.Tests/Infrastructure/Model/EfProductTests.cs
@@ -1,6 +1,8 @@
 using Catalog.Core.Model;
 using Catalog.Infrastructure.Model;
 
+using Shouldly;
+
 using Xunit;
 
 namespace Catalog.Tests.Infrastructure.Model;
@@ -19,5 +21,13 @@ public class EfProductTests
             Description = "SomeDesc",
             Name = "MyProduct"
         };
+        var subject = efproduct.ToProduct();
+
+        subject.ShouldNotBeNull();
+        subject.Id.ShouldBe(efproduct.ProductId);
+        subject.ProductName.ShouldBe(new ProductName(efproduct.Name));
+        subject.Description.ShouldBe(new ProductDescription(efproduct.Description));
+        subject.AvailableQuantity.ShouldBe(new AvailableQuantity(efproduct.AvailableQuantity));
+        subject.Price.ShouldBe(new ProductPrice(efproduct.Price, efproduct.PromotionalPrice));
     }
 }
