d422d79e89793575ce0974baf0278dfb48f9054b
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-05 19:42:27 +0100

    add reader tests

--- tests/Modules/Catalog.Tests/Infrastructure/Repositories/MartenProductReaderTests.cs
+++ tests/Modules/Catalog.Tests/Infrastructure/Repositories/MartenProductReaderTests.cs
@@ -33,6 +33,43 @@ public class MartenProductReaderTests : IClassFixture<PostgresSqlFixture>
     }
     
     [Fact]
+    public async Task ReadProductByIdsWhenNoExistsShouldReturnEmptyEnumerable()
+    {
+        // Arrange 
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromSeconds(30));
+        var productId = ProductId.New();
+
+        var repo = new EfCoreProductReader(_postgresSqlFixture.DbContext);
+        
+        // Act
+        
+        var subject = await repo.GetByIds(new [] { productId } , cts.Token).ToListAsync(cancellationToken: cts.Token);
+        
+        // Assert
+        subject.ShouldNotBeNull();
+        subject.ShouldBeEmpty();
+    }
+    
+    [Fact]
+    public async Task ReadProductsByIdsWhenNoExistsShouldReturnEmptyEnumerable()
+    {
+        // Arrange 
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromSeconds(30));
+
+        var repo = new EfCoreProductReader(_postgresSqlFixture.DbContext);
+        
+        // Act
+        
+        var subject = await repo.GetByIds(new [] { ProductId.New(), ProductId.New() } , cts.Token).ToListAsync(cancellationToken: cts.Token);
+        
+        // Assert
+        subject.ShouldNotBeNull();
+        subject.ShouldBeEmpty();
+    }
+    
+    [Fact]
     public async Task ReadProductByIdWhenExistsShouldReturnProduct()
     {
         // Arrange 
@@ -55,5 +92,33 @@ public class MartenProductReaderTests : IClassFixture<PostgresSqlFixture>
         subject.Price.ShouldBe(product.Price);
         subject.AvailableQuantity.ShouldBe(product.AvailableQuantity);
         subject.ProductDescription.ShouldBe(product.ProductDescription);
+    }
+    
+    [Fact]
+    public async Task ReadProductByIdsWhenExistsShouldReturnProduct()
+    {
+        // Arrange 
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromSeconds(30));
+        var productId = ProductId.New();
+        var product = new Product(productId, new ProductName("xDDD"), new ProductDescription("xDDD"),
+            new ProductPrice(new Price(10m), new Price(5m)), new AvailableQuantity(10));
+        
+        var repo = new EfCoreProductReader(_postgresSqlFixture.DbContext);
+        var writer = new EfCoreProductsWriter(_postgresSqlFixture.DbContext);
+        // Act
+
+        await writer.AddProduct(product, cts.Token);
+        var subject = await repo.GetByIds(new [] { productId } , cts.Token).ToListAsync(cancellationToken: cts.Token);
+        
+        subject.ShouldNotBeNull();
+        subject.ShouldNotBeEmpty();
+        subject.Count.ShouldBe(1);
+        var productFromDb = subject[0];
+        productFromDb.Id.ShouldBe(productId);
+        productFromDb.ProductName.ShouldBe(product.ProductName);
+        productFromDb.Price.ShouldBe(product.Price);
+        productFromDb.AvailableQuantity.ShouldBe(product.AvailableQuantity);
+        productFromDb.ProductDescription.ShouldBe(product.ProductDescription);
     }
 }
