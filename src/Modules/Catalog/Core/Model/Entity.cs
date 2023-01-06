c200872aebd4c5fa1fae163cfd8fb56769451696
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-06 17:08:22 +0100

    fix

--- src/Modules/Catalog/Core/Model/Entity.cs
+++ src/Modules/Catalog/Core/Model/Entity.cs
@@ -25,13 +25,9 @@ public readonly record struct Price(decimal Value)
     }
 }
 
-public sealed record ProductPrice(Price CurrentPrice, Price? PromotionalPrice = null)
-{
-}
+public sealed record ProductPrice(Price CurrentPrice, Price? PromotionalPrice = null);
 
 public readonly record struct AvailableQuantity(int Value);
 
 public sealed record Product(ProductId Id, ProductName ProductName, ProductDescription ProductDescription, ProductPrice Price,
-    AvailableQuantity AvailableQuantity)
-{
-}
+    AvailableQuantity AvailableQuantity);
