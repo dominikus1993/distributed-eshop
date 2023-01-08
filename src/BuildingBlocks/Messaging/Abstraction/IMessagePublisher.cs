f5c37ac7630423c6bb2ce9fc2fc6f0da1e2f2a21
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-08 11:44:55 +0100

    ctx

--- src/BuildingBlocks/Messaging/Abstraction/IMessagePublisher.cs
+++ src/BuildingBlocks/Messaging/Abstraction/IMessagePublisher.cs
@@ -1,10 +1,11 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Messaging.Abstraction;
 
 public interface IMessageContext
 {
-    
+    ActivityContext? Context { get; init; }
 }
 
 public interface IMessagePublisher<in T> where T : IMessage
