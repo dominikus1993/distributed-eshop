91ae803e7d92edc3234a6c2c31718c28e33c321c
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-14 17:41:30 +0100

    add catalog api fixture

---
+++ tests/Modules/Catalog.Tests/Fixtures/CatalogApiFixture.cs
@@ -0,0 +1,58 @@
+using Catalog.Infrastructure.DbContexts;
+
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace Catalog.Tests.Fixtures;
+
+public class CatalogApiFixture: IAsyncLifetime, IDisposable
+{
+    private readonly TestcontainerDatabaseConfiguration configuration = new PostgreSqlTestcontainerConfiguration("postgres:14-alpine") { Database = "posts", Username = "postgres", Password = "postgres" };
+
+    public PostgreSqlTestcontainer PostgreSql { get; }
+    public TestDbContextFactory DbContextFactory { get; private set; } = null!;
+    public ProductsDbContext DbContext { get; private set; } = null!;
+    public CatalogApiFixture()
+    {
+        this.PostgreSql = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+            .WithDatabase(this.configuration)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await this.PostgreSql.StartAsync()
+            .ConfigureAwait(false);
+        var builder = new DbContextOptionsBuilder<ProductsDbContext>()
+            .UseModel(ProductsDbContextModel.Instance)
+            .UseNpgsql(this.PostgreSql.ConnectionString,
+                optionsBuilder =>
+                {
+                    optionsBuilder.EnableRetryOnFailure(5);
+                    optionsBuilder.CommandTimeout(500);
+                    optionsBuilder.MigrationsAssembly("Catalog");
+                }).UseSnakeCaseNamingConvention();
+        var context = new ProductsDbContext(builder.Options);
+        DbContextFactory = new TestDbContextFactory(builder);
+        DbContext = context;
+        await context.Database.MigrateAsync();
+
+    }
+
+    public async Task DisposeAsync()
+    {
+        await this.PostgreSql.DisposeAsync()
+            .ConfigureAwait(false);
+        await DbContext.DisposeAsync();
+    }
+
+    public void Dispose()
+    {
+        this.configuration.Dispose();
+    }
+}
