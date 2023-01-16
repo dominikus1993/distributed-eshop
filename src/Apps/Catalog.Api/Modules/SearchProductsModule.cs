c60b9234d4434f9a4722c8a72bb4c0303920b85f
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-16 18:06:47 +0100

    small fix

--- src/Apps/Catalog.Api/Modules/SearchProductsModule.cs
+++ src/Apps/Catalog.Api/Modules/SearchProductsModule.cs
@@ -31,7 +31,7 @@ public sealed class SearchProductsModule : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/products", ([AsParameters]SearchProductsRequest msg) => TypedResults.Ok($"Hello {msg}"));
+        app.MapGet("/api/products", SearchProducts);
         app.MapGet("/api/products/{id:guid}", GetProductById);
     }
 
