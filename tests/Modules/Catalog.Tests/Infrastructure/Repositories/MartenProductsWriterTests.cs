1a973ab3bbb46e1b17c41dcc779f5209088065ed
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-27 18:11:01 +0100

    fix unit test

--- tests/Modules/Catalog.Tests/Infrastructure/Repositories/MartenProductsWriterTests.cs
+++ tests/Modules/Catalog.Tests/Infrastructure/Repositories/MartenProductsWriterTests.cs
@@ -17,11 +17,13 @@ namespace Catalog.Tests.Infrastructure.Repositories;
 public class MartenProductsWriterTests
 {
     private readonly PostgresSqlFixture _postgresSqlFixture;
+    private readonly IProductReader _productReader;
     private readonly IProductsWriter _productsWriter;
     public MartenProductsWriterTests(PostgresSqlFixture postgresSqlFixture)
     {
         _postgresSqlFixture = postgresSqlFixture;
         _productsWriter = new EfCoreProductsWriter(_postgresSqlFixture.DbContextFactory);
+        _productReader = new EfCoreProductReader(_postgresSqlFixture.DbContextFactory);
     }
 
     [Theory]
@@ -33,5 +35,26 @@ public class MartenProductsWriterTests
                     
         subject.ShouldNotBeNull();
         subject.IsT0.ShouldBeTrue();
+    }
+    
+    [Theory]
+    [InlineAutoData]
+    public async Task WriteProductTwoTimesTest(Product product, ProductName newProductName)
+    {
+        // Act
+        var subject = await _productsWriter.AddProduct(product);
+        
+        subject.ShouldNotBeNull();
+        subject.IsT0.ShouldBeTrue();
+
+        var newProduct = product with { ProductName = newProductName };
+        subject = await _productsWriter.AddProduct(newProduct);
+        
+        subject.ShouldNotBeNull();
+        subject.IsT0.ShouldBeTrue();
+        
+        var productFromDb = await _productReader.GetById(product.Id);
+        
+        productFromDb.ShouldBeEquivalentTo(newProduct);
     }
 }
