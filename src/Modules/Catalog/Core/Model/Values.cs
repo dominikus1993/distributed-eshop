6c39247d21abc3d61b951ad1a9955cd04f52ef60
Dominik.Kotecki <dominikus1910@gmail.com>
2023-09-23 18:23:59 +0200

    add opensearch

--- src/Modules/Catalog/Core/Model/Values.cs
+++ src/Modules/Catalog/Core/Model/Values.cs
@@ -5,4 +5,8 @@ public readonly record struct ProductId(Guid Value)
     public static ProductId From(Guid id) => new ProductId(id);
     
     public static ProductId New() => new ProductId(Guid.NewGuid());
+    
+    public static implicit operator Guid (ProductId id) => id.Value;
+    
+    public static implicit operator ProductId (Guid id) => new ProductId(id);
 }
---
+++ src/Modules/Catalog/Infrastructure/Model/OpenSearchProduct.cs
@@ -0,0 +1,44 @@
+using Catalog.Core.Model;
+
+namespace Catalog.Infrastructure.Model;
+
+internal sealed class OpenSearchProduct
+{
+    public ProductId ProductId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public decimal? PromotionalPrice { get; set; }
+
+    public decimal Price { get; set; }
+    
+    public int AvailableQuantity { get; set; }
+    
+    public IReadOnlyList<string>? Tags { get; set; }
+    
+    
+    public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.UtcNow;
+
+    public OpenSearchProduct()
+    {
+        
+    }
+
+    public OpenSearchProduct(Product product)
+    {
+        ProductId = product.Id;
+        Description = product.ProductDescription.Description;
+        Name = product.ProductName.Name;
+        AvailableQuantity = product.AvailableQuantity.Value;
+        Price = product.Price.CurrentPrice;
+        PromotionalPrice = product.Price.PromotionalPrice;
+        Tags = product.Tags?.Select(x => x.Name).ToArray();
+    }
+
+    public Product ToProduct()
+    {
+        var tags = Tags?.Select(tag => new Tag(tag)).ToArray() ?? Array.Empty<Tag>();
+        return new Product(ProductId, new ProductName(Name), new ProductDescription(Description),
+            new ProductPrice(Price, PromotionalPrice), new AvailableQuantity(AvailableQuantity), new Tags(tags));
+    }
+}
