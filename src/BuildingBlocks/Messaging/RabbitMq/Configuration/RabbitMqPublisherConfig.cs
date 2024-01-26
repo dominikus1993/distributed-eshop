701dc570e2cdaba7a73f9be7dd1c44089ce41827
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2024-01-26 15:22:47 +0100

    .

--- src/BuildingBlocks/Messaging/RabbitMq/Configuration/RabbitMqPublisherConfig.cs
+++ src/BuildingBlocks/Messaging/RabbitMq/Configuration/RabbitMqPublisherConfig.cs
@@ -10,4 +10,6 @@ public sealed class RabbitMqPublisherConfig<T>
 {
     public string Exchange { get; set; } = "eshop";
     public string Topic { get; set; } = "#";
+
+    public TimeSpan? TTL { get; set; } = null;
 }
